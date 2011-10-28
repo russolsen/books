a724ee5ebaa633fbb138a6d28678ef614f252526
Russ Olsen <russolsen@thinkrelevance.com>
2011-10-28 13:39:35 -0400

    New version of books database with a real homepage, seeded database, and some basic styling.

---
+++ books2/README.md
@@ -0,0 +1,11 @@
+##Books version 2
+
+This version of the books application adds a couple of things:
+ * It seeds the authors and publishers database with a couple of default values.
+ * Some very basic styling
+ * A home page http://localhost:3000 which just gives you links to the other pages.
+
+Russ
+
+
+
---
+++ books2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Books::Application.config.secret_token = 'baf7ac007d55f2b1690f7bbb4307c8abce307cbaccb957cfb38f838d5ac44e404b4aac27313d79eaf70874a64e51e3373ba42d6182a7756cfadef9f6d40ec33e'
